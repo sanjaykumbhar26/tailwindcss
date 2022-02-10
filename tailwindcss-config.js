7d9afd57e8e8f06a1f4abf112fd827ac22e83eb7
sanjay <sanjaykumbhar22@gmail.com>
2022-02-10 19:18:59 +0530

    first commit

---
+++ tailwindcss-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'bg-blueGray-50':'#f2f2f2',
+    },
+  },
+  plugins: [],
+}
